7d73689482c1c79a90b98f6ebc86e778862071a6
martaastatike <81478507+martaastatike@users.noreply.github.com>
2021-06-02 13:09:16 -0400

    Update template.js

--- template.js
+++ template.js
@@ -6,7 +6,7 @@ export default () => {
           <title>MERN Kickstart</title>
         </head>
         <body>
-          <div id="root"></div>
+          <div id="root"> Hello World</div>
         </body>
       </html>`
 }
