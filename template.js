a1b431b5713005cc1aa0ec882065acfe2ebe4b98
martaastatike <81478507+martaastatike@users.noreply.github.com>
2021-06-02 13:06:39 -0400

    Add files via upload

---
+++ template.js
@@ -0,0 +1,12 @@
+export default () => {
+    return `<!doctype html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <title>MERN Kickstart</title>
+        </head>
+        <body>
+          <div id="root"></div>
+        </body>
+      </html>`
+}
